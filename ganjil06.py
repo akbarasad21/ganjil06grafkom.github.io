08169ea7e24787fd125c236ed02087e76d130060
akbarasad21 <77686207+akbarasad21@users.noreply.github.com>
2021-12-07 18:13:37 +0800

    membuat perintah menginputkan nilai

---
+++ ganjil06.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as grafik
+
+x1 = int(input('Masukan x1 : '))
+y1 = int(input('Masukan y1 : '))
+x2 = int(input('Masukan x2 : '))
+y2 = int(input('Masukan y2 : '))
+

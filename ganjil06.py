9956d8c4d15333d1dded29196912c7015a465f3f
tegarsap <76468965+pielanakgaul@users.noreply.github.com>
2021-12-07 20:12:15 +0800

    Update ganjil06.py

--- ganjil06.py
+++ ganjil06.py
@@ -1,20 +1,29 @@
+# Numpy digunakan untuk memproses array
+# Matplotlib digunakan untuk memvisualisasikan data kordinat menjadi garis
+
 import numpy as np
 import matplotlib.pyplot as grafik
 
-x1 = int(input('Masukan x1 : '))
-y1 = int(input('Masukan y1 : '))
-x2 = int(input('Masukan x2 : '))
-y2 = int(input('Masukan y2 : '))
+# menginput koordinat (x1,y1) dan koordinat (x2,y2)
+x1 = int(input('Masukan nilai x1 : '))
+y1 = int(input('Masukan nilai y1 : '))
+x2 = int(input('Masukan nilai x2 : '))
+y2 = int(input('Masukan nilai y2 : '))
 
+# N adalah banyaknya iterasi yang dilakukan apabila x1 tidak sama dengan x2 atau y1 tidak sama dengan y2
 nilaiY = y2 - y1
 nilaiX = x2 - x1
 N = x2 - x1 + 1
 
+# titik awal x dan y
 x = x1
 y = y1
 
-i = 1
 
+# Jika x1 = x2 (garis vertikal), maka :
+# 1. y = y + 1 dan x tetap
+# 2. gambar titik kordinat (x,y) 
+# 3. tampilkan grafik 
 
 if x1 == x2:
     titikA = []
@@ -22,6 +31,11 @@ if x1 == x2:
     for i in range (1,y2,1):
         grafik.plot(titikA,titikB)
         grafik.show()
+        
+# Jika y1 = y2 (garis horizontal), maka :
+# 1. x = x + 1 dan y tetap
+# 2. gambar titik kordinat (x,y)
+# 3. tampilkan grafik
 
 elif y1 == y2:
     titikA = []
@@ -29,6 +43,14 @@ elif y1 == y2:
     for i in range (1,y2,1):
         grafik.plot(titikA,titikB)
         grafik.show()
+      
+# Jika 2 kondisi di atas salah, maka :
+# 1. hitung kemiringan garis dengan m = (y2 - y1) / (x2 - x1) 
+# 2. iterasi diulang sebanyak N
+# 3. y = m ( x - x1 ) + y1
+# 4. lakukan pembulatan pada y
+# 5. gambar titik (x, y(pembulatan))
+# 6. x = x + 1
         
 else:
     titikA = []
@@ -41,6 +63,10 @@ else:
         titikA.append(x)
         titikB.append(kordinatY)
         x+=1
+        
+# apppend digunakan untuk menambah item ke dalam array atau list
+# grafik.plot digunakan agar mathplotlib membuat titik pertemuan kordinat x,y
+# grafik.show digunakan agar mathplotlib menampilkan titik kordinat dari garis yang di lalui
         
     grafik.plot(titikA,titikB)
     grafik.show()
